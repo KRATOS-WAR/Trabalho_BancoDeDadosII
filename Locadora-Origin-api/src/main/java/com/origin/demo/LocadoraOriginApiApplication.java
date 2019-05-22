d11be80a490ad71e72a41d92f1c6ed0cfd7a6fb8
KRATOS-WAR <henriquepdes111man@gmail.com>
2019-05-22 13:41:52 -0300

    primeiro commit

---
+++ Locadora-Origin-api/src/main/java/com/origin/demo/LocadoraOriginApiApplication.java
@@ -0,0 +1,13 @@
+package com.origin.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LocadoraOriginApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LocadoraOriginApiApplication.class, args);
+	}
+
+}
